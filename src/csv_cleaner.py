612578690e815d3ea7b34cc77000415d11c3ceaf
Bismuth10K <bismuthdev@gmail.com>
2024-03-11 17:35:20 +0100

    suite des scripts
    csv cleaner nettoie les jeux de données et concatène les csv dans synop.csv
    Le jupyter notebook est le script contenant les réponses aux questions

--- src/csv_cleaner.py
+++ src/csv_cleaner.py
@@ -1,13 +1,22 @@
 import os
-
 import pandas as pd
 
 data_dir = "../Data/"
 datas = os.listdir(data_dir)
 
-for file in datas:
-	if file.endswith('.csv'):
-		print(file)
-		df = pd.read_csv(data_dir + file, sep=";")
-		df = df.replace('mq', None)
-		df.to_csv(data_dir + file, index=False)
+
+def clean_csv():
+	for file in datas:
+		if file.endswith('.csv') and file != "synop.csv":
+			print(file)
+			df = pd.read_csv(data_dir + file, sep=";")
+			df = df.replace('mq', None)
+			df.to_csv(data_dir + file, index=False)
+
+
+def concat_all_csv():
+	final_df = pd.concat([pd.read_csv(data_dir + file) for file in datas], ignore_index=True)
+	final_df.to_csv(data_dir + "synop.csv", index=False)
+
+
+concat_all_csv()
--- src/test_py.py
+++ src/test_py.py
@@ -1,10 +1,10 @@
+import numpy
 import pandas as pd
 import numpy as np
 
 data_dir = "../Data/"
 
-jan = pd.read_csv(data_dir + "synop.202301.csv", sep=";")
+jan = pd.read_csv(data_dir + "synop.202301.csv")
 
 print(jan)
 print(jan.loc[:, 't'])
-
